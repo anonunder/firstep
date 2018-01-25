0aec4f799bc482e245a16b5dc0cc92ce1ca43d13
Nikola R <anonunder@gmail.com>
2018-01-25 23:42:12 +0100

    Add files via upload

---
+++ user.php
@@ -0,0 +1,25 @@
+<?php
+
+class user extends entity{
+	protected static $table_fields = array('username','password');
+	protected static $table = "users";
+	protected static $class_name = __CLASS__;
+	public $id;
+	public $username;
+	public $password;
+
+	public static function verify_user($username,$password){
+		$db = db::getConnection()->conn;
+		$sql = "SELECT * FROM ".self::$table." WHERE username = :username AND password = :password";
+		$stmt = $db->prepare($sql);
+		$stmt->bindParam(':username',$username);
+		$stmt->bindParam(':password',$password);
+		$stmt->execute();
+		$user = $stmt->fetch(PDO::FETCH_OBJ);
+		return $user;
+
+	}
+
+	
+
+}
