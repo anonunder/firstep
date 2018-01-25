f7e259e0879dc5b3288e39340220d008971d2c07
Nikola R <anonunder@gmail.com>
2018-01-25 23:40:38 +0100

    Add files via upload
    <?php
    
    class user extends entity{
    	protected static $table_fields = array('username','password');
    	protected static $table = "users";
    	protected static $class_name = __CLASS__;
    	public $id;
    	public $username;
    	public $password;
    }

---
+++ entity.php
@@ -0,0 +1,94 @@
+<?php
+
+abstract class entity{
+
+	public static function find_all_users(){
+		$users = static::find_query("SELECT * FROM ".static::$table."");
+		$users->setFetchMode(PDO::FETCH_CLASS, static::$class_name);
+		$found_user = $users->fetchAll();
+		return $found_user;
+	}
+
+	public static function find_user_by_id($user_id){
+		$result_set = static::find_query("SELECT * FROM ".static::$table." where id = {$user_id} LIMIT 1");
+		$found_user = $result_set->fetchObject(static::$class_name);
+		return $found_user;
+	}
+
+	public static function find_query($sql){
+		$result_set = db::query($sql);
+		return $result_set;
+	}
+
+	protected function properties(){
+		$properties = array();
+		foreach (static::$table_fields as $db_field){
+			if(property_exists($this, $db_field)){
+				$properties[$db_field] = $this->$db_field;
+			}
+		}
+		return $properties;
+	}
+	public function create(){
+		$db = db::getConnection()->conn;
+		$properties = $this->properties();
+		$table_key = implode(",",array_keys($properties));
+		$table_value = implode(",:",array_keys($properties));
+		$sql = "INSERT INTO ".static::$table."(" . $table_key . ") ";
+		$sql.= "VALUES (:" . $table_value . ")";
+		$stmt = $db->prepare($sql);
+		foreach($properties as $key=>$value){
+			$stmt->bindParam($key,$value);
+		}
+		if($stmt->execute()){
+			print_r($stmt);
+			$this->id = db::the_insert_id();
+			return true;
+		}else{
+			print_r($stmt);
+			return false;
+		}
+		return $stmt;
+	}
+
+	public function delete(){
+		$db = db::getConnection()->conn;
+		$sql = "DELETE FROM ".self::$table." WHERE ";
+		$sql .= "id = :id ";
+		$sql .= "LIMIT 1";
+		$stmt = $db->prepare($sql);
+		$stmt->bindParam(':id', $this->id, PDO::PARAM_INT);
+		if($stmt->execute()){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	public function update(){
+		$db = db::getConnection()->conn;
+		$properties = $this->properties();
+		$table_key = implode(",",array_keys($properties));
+		$table_value = implode(",:",array_keys($properties));
+
+		$properties_pairs = array();
+
+		foreach($properties as $key => $value){
+			$properties_pairs[] = "{$key}=:$key";
+		}
+		
+		$sql = "UPDATE " .static::$table." SET ";
+		$sql .= implode(", ",$properties_pairs);
+		$sql .= " where id = :id";
+		$stmt = $db->prepare($sql);
+		foreach($properties as $key=>$value){
+			$asd = $stmt->bindParam($key,$value);
+		}
+		$stmt->bindParam(':id', $this->id, PDO::PARAM_INT);
+		if($stmt->execute()){
+			echo "da";
+		}else{
+			echo "ne";
+		}
+	}
+}
